25f2f2d190eee988a679500266e66a6a52d7fd27
LassilaJoni <joni@jonilassila.com>
2022-05-19 11:54:20 +0300

    Initial Commit

---
+++ SieniKirja/SieniKirjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SieniKirjaApp.swift
+//  SieniKirja
+//
+//  Created by Joni Lassila on 19.5.2022.
+//
+
+import SwiftUI
+
+@main
+struct SieniKirjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
